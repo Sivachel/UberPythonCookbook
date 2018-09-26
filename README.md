e07ae743fd3cc2bde7fcb7f009aca386585b6c6a
Sivachel <lsivachelvan@spartaglobal.com>
2018-09-26 11:43:59 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# UberPythonCookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: UberPythonCookbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
